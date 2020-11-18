f9359d5735453f78ab3376503a3a20bd4aa844fc
dnakano <dnakano@student.42tokyo.jp>
2020-11-18 18:07:02 +0900

    Update: Add user for wp to db.

--- srcs/mysql/srcs/start.sh
+++ srcs/mysql/srcs/start.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
 # If no mysql data directory (means firsttime launch), initialize.
+##  MEMO:
+##      FLUSH
 if [ ! -d /run/mysqld ]; then
 echo [database] initializing...
+DB_SYSPASS=`pwgen 32 1`
     cat << EOF > init.sql
 USE mysql;
 FLUSH PRIVILEGES ;
-GRANT ALL ON *.* TO 'root'@'%' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-GRANT ALL ON *.* TO 'root'@'localhost' identified by '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION ;
-SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${MYSQL_ROOT_PASSWORD}') ;
+GRANT ALL ON *.* TO 'root'@'%' identified by '$DB_ROOTPASS' WITH GRANT OPTION ;
+GRANT ALL ON *.* TO 'root'@'localhost' identified by '$DB_ROOTPASS' WITH GRANT OPTION ;
+SET PASSWORD FOR 'root'@'localhost'=PASSWORD('${DB_ROOTPASS}') ;
+SET PASSWORD FOR 'mariadb.sys'@'localhost'=PASSWORD('${DB_SYSPASS}') ;
 DROP DATABASE IF EXISTS test ;
+CREATE DATABASE IF NOT EXISTS \`$WP_DBNAME\` CHARACTER SET utf8 COLLATE utf8_general_ci;
+GRANT ALL ON \`$WP_DBNAME\`.* to '$WP_DBUSER'@'$WP_DBHOST' IDENTIFIED BY '$WP_DBPASS';
+DELETE FROM mysql.user WHERE Password="";
 FLUSH PRIVILEGES ;
 EOF
     /usr/bin/mysqld --user=mysql --bootstrap < init.sql
     rm init.sql
 fi
 
-echo [database] starting...
+echo [mysql] starting...
 /usr/bin/mysqld_safe --datadir='/var/lib/mysql'
--- srcs/wordpress/srcs/start.sh
+++ srcs/wordpress/srcs/start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+sed -e s/database_name_here/$WP_DBNAME/ \
+    -e s/username_here/$WP_DBUSER/ \
+    -e s/password_here/$WP_DBPASS/ \
+    /var/lib/nginx/html/wp-config-sample.php \
+    > /var/lib/nginx/html/wp-config.php
+
 # Start nginx and php-fpm
 echo [wordpress] starting...
 php-fpm7
